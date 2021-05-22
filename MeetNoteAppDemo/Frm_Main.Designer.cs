c214c5bcb48c59d2cedfccf95d4109c2798f01f9
士勛 翁 <wong683837@gmail.com>
2021-05-22 17:19:14 +0800

    MiddleApp_MeeNoteApp_v1.1

---
+++ MeetNoteAppDemo/Frm_Main.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace MeetNoteApp
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Main));
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_Clear.Font = new System.Drawing.Font("宋体", 10F);
+            this.btn_Clear.Image = ((System.Drawing.Image)(resources.GetObject("btn_Clear.Image")));
+            this.btn_Clear.Location = new System.Drawing.Point(363, 37);
+            this.btn_Clear.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(200, 67);
+            this.btn_Clear.TabIndex = 7;
+            this.btn_Clear.UseVisualStyleBackColor = false;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(34, 169);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(976, 548);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("宋体", 10F);
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(11, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 52);
+            this.button1.TabIndex = 9;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Font = new System.Drawing.Font("宋体", 10F);
+            this.btn_Save.Location = new System.Drawing.Point(5114, 200);
+            this.btn_Save.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(1528, 828);
+            this.btn_Save.TabIndex = 6;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Visible = false;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(799, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 67);
+            this.button2.TabIndex = 10;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(578, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 67);
+            this.button3.TabIndex = 11;
+            this.button3.Text = " ";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 15F);
+            this.label4.Location = new System.Drawing.Point(28, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(285, 35);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "MeeNoteApp_v1.1";
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1920, 1040);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Save);
+            this.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Frm_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MeeNoteApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MeetNoteAppDemo/Frm_Main.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using WebSocketSharp;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Runtime.InteropServices;
+
+
+
+namespace MeetNoteApp
+{
+    public partial class Frm_Main : Form
+    {
+
+
+
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int cmdShow);
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+        private const int WS_SHOWNORMAL = 1;
+
+        public Frm_Main()
+        {
+           
+
+            InitializeComponent();
+
+
+
+        }
+
+        //记录直线或者曲线的对象
+        private System.Drawing.Drawing2D.GraphicsPath mousePath = new System.Drawing.Drawing2D.GraphicsPath();
+        //画笔透明度
+        private int myAlpha = 100;
+        //画笔颜色对象
+        private Color myUserColor = new Color();
+        //画笔宽度
+        private int myPenWidth = 3;
+        //签名的图片对象
+        public Bitmap SavedBitmap;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           /* //獲取當前活動進程的模塊名稱
+            string moduleName = System.Diagnostics.Process.GetCurrentProcess().MainModule.ModuleName;
+            //返回指定路徑字符串的文件名
+            string processName = System.IO.Path.GetFileNameWithoutExtension(moduleName);
+            //根據文件名創建進程資源數組
+            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName(processName);
+            //如果該數組長度大於1，說明多次運行
+            if (processes.Length > 1)
+            {
+                //MessageBox.Show("本程序一次只能運行一個實例！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);//彈出提示信息
+                this.Close();//關閉當前窗體
+            }*/
+
+            showOnMonitor(1);
+        }
+
+        void showOnMonitor(int showOnMonitor)
+        {
+            Screen[] sc;
+            sc = Screen.AllScreens;
+            //get all the screen width and heights 
+            
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Left = sc[0].Bounds.Width;
+            this.Top = 0;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Show();
+            this.WindowState= System.Windows.Forms.FormWindowState.Maximized;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                try
+                {
+                    mousePath.AddLine(e.X, e.Y, e.X, e.Y);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+
+                mousePath.StartFigure();
+                mousePath.AddLine(e.X, e.Y, e.X, e.Y);
+            }
+        }
+
+        #region 参数设置
+        public void set()
+        {
+            //画笔宽度
+            myPenWidth = 2;
+            //myUserColor = System.Drawing.Color.Blue;
+            //myAlpha = 100;
+        }
+        #endregion
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            try
+            {
+               
+
+                set();//设置画笔的颜色、宽度、透明度
+                Pen CurrentPen = new Pen(Color.Black, myPenWidth);
+                e.Graphics.DrawPath(CurrentPen, mousePath);
+            }
+            catch { }
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.CreateGraphics().Clear(Color.White);
+            mousePath.Reset();
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            bool isSave = true;
+            SavedBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(SavedBitmap, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+           
+            #region 保存图片
+            SaveFileDialog saveImageDialog = new SaveFileDialog();
+            saveImageDialog.Title = "图片保存";
+            saveImageDialog.Filter = @"jpeg|*.jpg|bmp|*.bmp|gif|*.gif";
+            if (saveImageDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = saveImageDialog.FileName.ToString();
+                if (fileName != "" && fileName != null)
+                {
+                    string fileExtName = fileName.Substring(fileName.LastIndexOf(".") + 1).ToString();
+                    System.Drawing.Imaging.ImageFormat imgformat = null;
+                    //默认保存为JPG格式   
+                    if (imgformat == null)
+                    {
+                        imgformat = System.Drawing.Imaging.ImageFormat.Jpeg;
+                    }
+                    if (isSave)
+                    {
+                        try
+                        {
+                            SavedBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                            SavedBitmap.Dispose();
+                            MessageBox.Show("图片已经成功保存!");   
+                        }
+                        catch
+                        {
+                            MessageBox.Show("保存失败,你还没有截取过图片或已经清空图片!");
+                        }
+                    }
+                }
+            }
+            #endregion
+        }
+
+        public Bitmap SaveImage(PictureBox pictureBox1)
+        {
+            Bitmap SavedBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(SavedBitmap, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+            return SavedBitmap;
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SavedBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(SavedBitmap, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+            Clipboard.SetImage(SavedBitmap);
+        }
+
+
+
+        public static void HandleRunningInstance(System.Diagnostics.Process instance)
+        {
+            // 相同時透過ShowWindowAsync還原，以及SetForegroundWindow將程式至於前景
+            ShowWindowAsync(instance.MainWindowHandle, WS_SHOWNORMAL);
+            SetForegroundWindow(instance.MainWindowHandle);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool BringWindowToTop(IntPtr hWnd);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(String lpClassName, String lpWindowName);
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+           
+            if (System.Diagnostics.Process.GetProcessesByName("OUTLOOK").Length > 0)
+            {
+                HandleRunningInstance(System.Diagnostics.Process.GetProcessesByName("OUTLOOK")[0]);
+
+               
+            }
+            else
+            {
+                Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey("Software\\microsoft\\windows\\currentversion\\app paths\\OUTLOOK.EXE");
+                string path = (string)key.GetValue("Path");
+                if (path != null)
+                {
+                    System.Diagnostics.Process.Start("OUTLOOK.EXE");
+
+                  
+                }
+                else
+                    MessageBox.Show("There is no Outlook in this computer!", "SystemError", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+             
+            }
+
+           // OpenOutlook();
+
+            using (var ws = new WebSocket("ws://localhost:13812"))
+            {
+                ws.OnMessage += (s, e1) =>
+                {
+                    Console.WriteLine("Laputa says: " + e1.Data);
+                    
+                };
+                    
+                ws.Connect();
+                ws.Send("{\"Status\":\"setting\",\"Mode\":\"3\"," +
+                    "\"Handwriting\":\"1\",\"Gamma\":\"4\",\"PenWidth\":" +
+                    "\"3\",\"EraserWidth\":\"10\",\"DisplayMode\":\"1\"}");
+                //Console.ReadKey(true);
+
+
+            }
+
+
+            //讓程式在工具列中隱藏
+            this.ShowInTaskbar = false;
+            //隱藏程式本身的視窗
+            this.Hide();
+
+            System.Environment.Exit(System.Environment.ExitCode);
+
+
+
+
+
+        }
+
+
+        static void OpenOutlook()
+        {
+
+            System.Diagnostics.Process[] telerecProcs = System.Diagnostics.Process.GetProcessesByName("OUTLOOK.EXE");
+            if (telerecProcs.Length > 0)
+            {
+                bringToFront("OUTLOOK"); //填入視窗的Title Name                      
+            }
+            else
+            {
+                System.Diagnostics.Process sample = new System.Diagnostics.Process();
+                sample.StartInfo.FileName = "OUTLOOK.EXE";
+                sample.Start();
+            }
+
+            /*Outlook.Application outlookObj = null;
+
+            if (System.Diagnostics.Process.GetProcessesByName("OUTLOOK").Count().Equals(0))
+            {
+                System.Diagnostics.Process.Start("OUTLOOK.EXE");
+            }
+            else
+            {
+                outlookObj = (Outlook.Application)Marshal.GetActiveObject("Outlook.Application");
+               
+            }*/
+        }
+
+        public static void bringToFront(string title)
+        {
+            // Get a handle to the Calculator application.
+            IntPtr handle = FindWindow(null, title);
+            // Verify that Calculator is a running process.
+            if (handle == IntPtr.Zero)
+            {
+                return;
+            }
+            BringWindowToTop(handle); // 將視窗浮在最上層
+            ShowWindow(handle, 3); // 將視窗最大化
+        }
+
+    }
+}
